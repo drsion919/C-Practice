10300fc29377977102fe256957283132581db584
Seeon Kim <drsion919@gmail.com>
2016-07-17 20:40:10 +0900

    Update Comment

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+ * File: HelloWorld.c
+ * Description: prinf how to, git, main 함수, git clone, commit and push
+ * by drsion919
+ * */
 
 int main() {
     printf("Hello World");
